4d89e03dbcae22bcd386506b4654e9fece8d9429
unknown <dnl.rammer@gmail.com>
2016-02-07 18:04:08 +0100

    no message

---
+++ XBoxInput/Assets/InputProcessing/Keyboard/KeyboardInputProcessor.cs
@@ -0,0 +1,41 @@
+﻿using XInputDotNetPure;
+using InputProcessing;
+using UnityEngine;
+
+public class KeyboardInputProcessor : InputProcessor {
+    /// <summary>
+    /// Input processor for a XBox360 gamepad using XInputDotNetPure.dll.
+    /// The XBox button states are transfered to the virtual controller ones.
+    /// </summary>
+    /// <param name="playerNumber"></param>
+    public KeyboardInputProcessor(int playerNumber) : base(playerNumber) {
+        if (playerNumber < 0)
+            playerNumber = 0;
+        else if (playerNumber > 3)
+            playerNumber = 3;
+
+        mapping = new KeyboardMapping(playerNumber);
+        InitializeButtonsAndAxes();
+    }
+
+    public override void Update() {
+
+        foreach (DelegateToVirtualButton item in buttons) {
+            if (item.ButtonDelegate()) { // button is held
+                if (item.VirtualState == VirtualButtonState.Down)
+                    item.VirtualState = VirtualButtonState.Hold;
+                else if (item.VirtualState == VirtualButtonState.Released || item.VirtualState == VirtualButtonState.Up)
+                    item.VirtualState = VirtualButtonState.Down;
+            }
+            else if (!item.ButtonDelegate()) { // button is released
+                if (item.VirtualState == VirtualButtonState.Up)
+                    item.VirtualState = VirtualButtonState.Released;
+                else if (item.VirtualState == VirtualButtonState.Hold | item.VirtualState == VirtualButtonState.Down)
+                    item.VirtualState = VirtualButtonState.Up;
+            }
+        }
+        for (int i = 0; i < axes.Count; i++) {
+            axes[i].Process();
+        }
+    }
+}
---
+++ XBoxInput/Assets/InputProcessing/XBox360/XBox360Mapping.cs
@@ -0,0 +1,43 @@
+﻿using XInputDotNetPure;
+using InputProcessing;
+/// <summary>
+/// maps delegates to actual button and axis states of the xbox 360 controller
+/// </summary>
+public class XBox360Mapping : GamePadMapping {
+    public XBox360Mapping(int playerNumber) : base(playerNumber) {
+        playerIndex = playerNumber;
+        InitializeGamepad();
+    }
+
+    private bool BtnStateToBool(ButtonState state) {
+        return state == ButtonState.Pressed;
+    }
+
+    protected override void InitializeGamepad() {
+        leftStickY = x => GamePad.GetState((PlayerIndex)playerIndex).ThumbSticks.Left.Y;
+        leftStickX = x => GamePad.GetState((PlayerIndex)playerIndex).ThumbSticks.Left.X;
+        rightStickY = x => GamePad.GetState((PlayerIndex)playerIndex).ThumbSticks.Right.Y;
+        rightStickX = x => GamePad.GetState((PlayerIndex)playerIndex).ThumbSticks.Right.X;
+
+        leftTrigger = x => GamePad.GetState((PlayerIndex)playerIndex).Triggers.Left;
+        rightTrigger = x => GamePad.GetState((PlayerIndex)playerIndex).Triggers.Right;
+
+        faceDown = x => BtnStateToBool(GamePad.GetState((PlayerIndex)playerIndex).Buttons.A);
+        faceRight = x => BtnStateToBool(GamePad.GetState((PlayerIndex)playerIndex).Buttons.B);
+        faceLeft = x => BtnStateToBool(GamePad.GetState((PlayerIndex)playerIndex).Buttons.X);
+        faceUp = x => BtnStateToBool(GamePad.GetState((PlayerIndex)playerIndex).Buttons.Y);
+
+        buttonDLeft = x => BtnStateToBool(GamePad.GetState((PlayerIndex)playerIndex).DPad.Left);
+        buttonDRight = x => BtnStateToBool(GamePad.GetState((PlayerIndex)playerIndex).DPad.Right);
+        buttonDDown = x => BtnStateToBool(GamePad.GetState((PlayerIndex)playerIndex).DPad.Down);
+        buttonDUP = x => BtnStateToBool(GamePad.GetState((PlayerIndex)playerIndex).DPad.Up);
+
+        buttonLeftSchoulder = x => BtnStateToBool(GamePad.GetState((PlayerIndex)playerIndex).Buttons.LeftShoulder);
+        buttonRightShoulder = x => BtnStateToBool(GamePad.GetState((PlayerIndex)playerIndex).Buttons.RightShoulder);
+        buttonLeftTrigger = x => BtnStateToBool(GamePad.GetState((PlayerIndex)playerIndex).Buttons.LeftShoulder);
+        buttonRightTrigger = x => BtnStateToBool(GamePad.GetState((PlayerIndex)playerIndex).Buttons.RightShoulder);
+
+        buttonStart = x => BtnStateToBool(GamePad.GetState((PlayerIndex)playerIndex).Buttons.Start);
+        buttonBack = x => BtnStateToBool(GamePad.GetState((PlayerIndex)playerIndex).Buttons.Back);
+    }
+}
